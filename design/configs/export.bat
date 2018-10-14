695782acdb5eef20ba838063f6c3b0733845acbb
KevinCaiqimin <caiqimin_kevin@163.com>
2018-10-14 16:38:42 +0800

    config tables

---
+++ design/configs/export.bat
@@ -0,0 +1,3 @@
+xtool.exe -srcdir=tbls -js -jsdir=../../hbclient/assets/scritps/configs -py -pydir=../../server/gateway_service/logic/configs
+xtool.exe -srcdir=tbls -py -pydir=../../server/logic_service/logic/configs
+pause
